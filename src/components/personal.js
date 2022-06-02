37464abacbf5ce7b5cfa21f713d1dc58f6b8d0f4
Nileshgangwar <nileshgangwar20@gmail.com>
2022-06-02 12:11:10 +0530

    link

---
+++ src/components/personal.js
@@ -0,0 +1,15 @@
+import React ,{Component} from 'react';
+
+
+class Persone extends Component {
+
+    render(){
+        return(
+            <div>
+                 <h1>Nilesh Kumar</h1>
+            </div>
+        );
+    }
+}
+
+export default Persone;
